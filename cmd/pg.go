ead4cadbfdd0780313e40b0a7bf2d37269c4b667
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-04-09 19:00:49 +0200

    refactor: pg logic 🏗️

--- cmd/pg.go
+++ cmd/pg.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"github.com/kareemmahlees/meta-x/internal"
+	"github.com/kareemmahlees/meta-x/internal/db"
+	"github.com/kareemmahlees/meta-x/lib"
+	"github.com/kareemmahlees/meta-x/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -8,48 +12,55 @@ var pgCommand = &cobra.Command{
 	Use:   "pg",
 	Short: "use postgres as the database provider",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// var cfg string
+		var pgConfig utils.PgConfig
 
-		// connUrl, _ := cmd.Flags().GetString("url")
-		// if connUrl != "" {
-		// 	cfg = connUrl
-		// } else {
-		// 	dbUsername, _ := cmd.Flags().GetString("username")
-		// 	dbHost, _ := cmd.Flags().GetString("host")
-		// 	dbPort, _ := cmd.Flags().GetInt("dbPort")
-		// 	dbName, _ := cmd.Flags().GetString("db")
-		// 	dbSslMode, _ := cmd.Flags().GetString("sslmode")
+		port, _ := cmd.Flags().GetInt("port")
 
-		// 	dbPassword, _ := cmd.Flags().GetString("password")
-		// 	if dbPassword == "" {
-		// 		fmt.Println("Enter password: ")
-		// 		fmt.Scanln(&dbPassword)
-		// 	}
+		connUrl, _ := cmd.Flags().GetString("url")
+		if connUrl != "" {
+			pgConfig = utils.PgConfig{ConnUrl: &connUrl, ConnParams: nil}
+		} else {
+			dbUsername, _ := cmd.Flags().GetString("username")
+			dbHost, _ := cmd.Flags().GetString("host")
+			dbPort, _ := cmd.Flags().GetInt("dbPort")
+			dbName, _ := cmd.Flags().GetString("db")
+			dbSslMode, _ := cmd.Flags().GetString("sslmode")
+			dbPassword, _ := cmd.Flags().GetString("password")
 
-		// 	cfg, _ = pq.ParseURL(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s", dbUsername, dbPassword, dbHost, dbPort, dbName, dbSslMode))
-		// }
+			pgConfig = utils.PgConfig{ConnUrl: nil, ConnParams: &utils.PgConnectionParams{
+				DbUsername: dbUsername,
+				DbPassword: dbPassword,
+				DbHost:     dbHost,
+				DbPort:     dbPort,
+				DbName:     dbName,
+				DbSslMode:  dbSslMode,
+			}}
+		}
+		conn, err := internal.InitDBConn(lib.PSQL, pgConfig)
+		if err != nil {
+			return err
+		}
+		provider := db.NewPgProvider(conn)
+		server := internal.NewServer(provider, port, make(chan<- bool))
 
-		// port, _ := cmd.Flags().GetInt("port")
+		if err := server.Serve(); err != nil {
+			return err
+		}
 
-		// app := fiber.New(fiber.Config{DisableStartupMessage: true})
-
-		// if err := internal.InitDBAndServer(app, lib.PSQL, cfg, port, make(chan bool, 1)); err != nil {
-		// 	return err
-		// }
 		return nil
 	},
 }
 
 func init() {
 
-	pgCommand.Flags().String("username", "", "db username")
+	pgCommand.Flags().String("username", "postgres", "db username")
 	pgCommand.Flags().String("password", "", "db password")
 	pgCommand.Flags().String("host", "localhost", "db host")
 	pgCommand.Flags().Int("dbPort", 5432, "db port")
 	pgCommand.Flags().String("db", "postgres", "db name")
 	pgCommand.Flags().String("url", "", "connection url/string")
 	pgCommand.Flags().String("sslmode", "disable", "db sslmode")
-	pgCommand.MarkFlagsMutuallyExclusive("username", "url")
+	pgCommand.MarkFlagsMutuallyExclusive("password", "url")
 
 	rootCmd.AddCommand(pgCommand)
 }
--- utils/config.go
+++ utils/config.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"fmt"
+
+	"github.com/lib/pq"
+)
+
 type Config interface {
 	DSN() string
 }
@@ -12,6 +18,40 @@ func NewSQLiteConfig(filePath string) *SQLiteConfig {
 	return &SQLiteConfig{filePath}
 }
 
-func (sc *SQLiteConfig) DSN() string {
+func (sc SQLiteConfig) DSN() string {
 	return sc.filePath
+}
+
+type PgConfig struct {
+	ConnUrl    *string
+	ConnParams *PgConnectionParams
+}
+
+type PgConnectionParams struct {
+	DbUsername string
+	DbHost     string
+	DbPort     int
+	DbName     string
+	DbSslMode  string
+	DbPassword string
+}
+
+func NewPGConfig(connUrl *string, pgConnParams *PgConnectionParams) *PgConfig {
+	return &PgConfig{connUrl, pgConnParams}
+}
+
+func (pc PgConfig) DSN() string {
+	if *pc.ConnUrl != "" {
+		return *pc.ConnUrl
+	} else {
+		cfg, _ := pq.ParseURL(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+			pc.ConnParams.DbUsername,
+			pc.ConnParams.DbPassword,
+			pc.ConnParams.DbHost,
+			pc.ConnParams.DbPort,
+			pc.ConnParams.DbName,
+			pc.ConnParams.DbSslMode,
+		))
+		return cfg
+	}
 }
